1f28e1bb98dff316f21aff98e30a0656a6d929d8
Sai Anudeep Bhogaraju <anudeepbhogaraju.64@gmail.com>
2022-02-19 00:32:52 +0530

    Added all the stuff

--- carzone/settings.py
+++ carzone/settings.py
@@ -19,6 +19,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'cars.apps.CarsConfig',
     'pages.apps.PagesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -26,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'multiselectfield',
 ]
 
 MIDDLEWARE = [
@@ -109,6 +111,9 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'carzone/static'),
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Team
 
-# Register your models here.
+
+class Teamadmin(admin.ModelAdmin):
+    list_display = ('id','first_name','last_name',"created_at")
+
+admin.site.register(Team,Teamadmin)
